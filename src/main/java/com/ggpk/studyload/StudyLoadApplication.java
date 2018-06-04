ed0ed25a6448f4f31804cd079dc8b6e6ca9efad7
roman.kuhta <romakuhta@mail.com>
2018-06-04 12:36:04 +0300

    Dev Bugfix Fix locale bug

--- src/main/java/com/ggpk/studyload/StudyLoadApplication.java
+++ src/main/java/com/ggpk/studyload/StudyLoadApplication.java
@@ -3,15 +3,29 @@ package com.ggpk.studyload;
 import com.ggpk.studyload.ui.HomeView;
 import com.ggpk.studyload.ui.LoadingView;
 import de.felixroske.jfxsupport.AbstractJavaFxApplicationSupport;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
 
 
 @SpringBootApplication
 @EnableJpaRepositories
+@Slf4j
 public class StudyLoadApplication extends AbstractJavaFxApplicationSupport {
 
+    public static final String LANG_LANGUAGE = "lang.language";
+
     public static void main(String[] args) {
+
+        try {
+            ResourceBundle.getBundle(LANG_LANGUAGE, Locale.getDefault());
+        } catch (Exception e) {
+            log.error("Error to find bundle for this locale, get default bundle", e.getMessage());
+            Locale.setDefault(new Locale.Builder().setLanguage("ru").setRegion("RU").build());
+        }
         launch(StudyLoadApplication.class, HomeView.class, new LoadingView(), args);
 
     }
--- src/main/java/com/ggpk/studyload/config/ApplicationConfiguration.java
+++ src/main/java/com/ggpk/studyload/config/ApplicationConfiguration.java
@@ -1,6 +1,7 @@
 package com.ggpk.studyload.config;
 
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,22 +10,23 @@ import org.springframework.context.support.ResourceBundleMessageSource;
 import java.util.Locale;
 import java.util.ResourceBundle;
 
+import static com.ggpk.studyload.StudyLoadApplication.LANG_LANGUAGE;
+
 @Configuration
+@Slf4j
 public class ApplicationConfiguration {
 
 
     @Bean
     public ResourceBundle resourceBundle() {
-        return ResourceBundle.getBundle("lang.language", Locale.getDefault());
+        return ResourceBundle.getBundle(LANG_LANGUAGE, Locale.getDefault());
     }
 
     @Bean
     public MessageSource messageSource() {
         ResourceBundleMessageSource source = new ResourceBundleMessageSource();
-        source.setBasename("lang.language");
+        source.setBasename(LANG_LANGUAGE);
         source.setDefaultEncoding("UTF-8");
         return source;
     }
-
-
 }
